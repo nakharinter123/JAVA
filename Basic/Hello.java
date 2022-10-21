dd5010db6ae5113ca4175ab1612e3803dae61abb
nakharinter123 <ipangopqy@gmail.com>
2022-10-21 11:07:08 +0700

    pang52
    52

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Helloworld!");
+        System.out.println("my name is pang");
+        System.out.println("555555");
     }
 }
